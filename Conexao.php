6fc1d77731e3a46d2b37961ea6edbf7693c4582f
SarahElizabeteTDS <130590547+SarahElizabeteTDS@users.noreply.github.com>
2025-05-29 23:53:37 -0300

    Add files via upload

---
+++ Conexao.php
@@ -0,0 +1,23 @@
+<?php
+
+class Conexao
+{
+    private $server = "localhost";
+    private $banco = "bibliotecafw";
+    private $usuario = "root";
+    private $senha = "";
+
+    function conectar()
+    {
+        try
+        {
+            $con = new PDO("mysql:host=" . $this->server . ";dbname=" . $this->banco, $this->usuario, $this->senha);
+            return $con;
+        }catch(Exception $e)
+        {
+            print "Erro ao conectar com o banco de dados: " . $e->getMessage();
+        }
+    }
+}
+
+(new Conexao())->conectar();
